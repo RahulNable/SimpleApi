07eda344891235f22dc390c50c60c4a76ebd065b
JayPrakash-Nable <jayprakash.bhardwaj@nableinvent.com>
2022-12-09 16:51:28 +0530

    added readme and mysql.whl file

--- fileupload/file_app/urls.py
+++ fileupload/file_app/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from .views import FileView, extractFile, getFileNames, toDataBase
+from .views import FileView, extractFile, getFileNames
 
 urlpatterns = [
     url(r'^upload/$', FileView.as_view(), name='file-upload'),
     url(r'^data/$', extractFile, name='extract'),
     url(r'^filelist/$', getFileNames, name='filelist'),
-    url(r'^savedata/$', toDataBase, name='save'),
 ]
